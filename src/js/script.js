caf47d2d550e9c08d2fb3a2969999b3d2f594ec1
Fle1ta <grey5boyko@yndex.ru>
2020-10-16 21:02:50 +0300

    added alternate sliders

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.carusel__inner').slick({
         speed: 1200,
         //adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.png"</button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.png"</button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.png"></button>',
         responsive: [{
                         breakpoint: 992,
                         settings: {
@@ -13,3 +13,20 @@ $(document).ready(function(){
                     }]
       });
   });
+
+// let slider = tns({
+//     container: '.carusel__inner',
+//     items: 1,
+//     slideBy: 'page',
+//     autoplay: false,
+//     nav:false,
+//     controls: false
+// });
+
+// document.querySelector('.prev').onclick = function () {
+//     slider.goTo("prev");
+// };
+
+// document.querySelector('.next').onclick = function () {
+//     slider.goTo("next");
+// };
